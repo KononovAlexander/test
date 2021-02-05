227f8e95bb645e9c229476cdb6ad83e981d2c8d1
KononovAlexander <kononov8214@gmail.com>
2021-02-05 14:26:59 +0200

    chert pobery rekursiu & stack

--- script.js
+++ script.js
@@ -6,37 +6,56 @@ let gameBot = function(bool){
     let userNum;
     let number = 87;
     
+
     let game = function(a, b){
         if(a * 1 === b){
-            alert('Угадал!');
+
+            alert('"Поздравляю, Вы угадали!!!"');
             gameBot(false);
+
         }
         else if(a * 1 > b){
-            alert('Меньше!');
+
+            alert(' "Загаданное число меньше" ');
             gameBot(true);
+
         }else{
-            alert('Больше!');
+
+            alert('"Загаданное число больше"');
             gameBot(true);
+
         } 
     }; 
 
+
     let isNumber = function(num){
+
         if(num === null){
+
            gameBot(false);
+
         }else if(isNaN(num)){
-            alert('Введи число!');
+
+            alert(' "Введи число!"');
             gameBot(true);
+
         }else{
+
             game(userNum, number);
+
         }
     };
 
+
     if(bool === false){
+        
         alert('Игра окончена!!!');
+
        }else{
+
           isNumber(userNum = prompt('Угадай число от 1 до 100'));
+
        }
-       
     };
     
     gameBot(result);
